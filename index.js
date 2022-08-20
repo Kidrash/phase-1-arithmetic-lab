bb790b01b239db2c9083c51d5b8a55730dafcaf5
Kidrash <markkaranja11@gmail.com>
2022-08-20 12:54:17 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = (num1*num2);
+ console.log (multiply);
+
+ const random = Math.floor(Math.random() * 10) + 1;
+ random > 0;
+
+ let num3 = 7;
+ let num4 = 74;
+ const mod = (num4 % num3);
+ console.log(mod);
+
+ const max = Math.max(14, 7, 20, 1);
+ console.log(max);
